369a07ef11ad81bc4b4c2022402291ca2df62ef7
yuxil2 <yuxil2@andrew.cmu.edu>
2023-09-09 01:05:41 -0700

    Implemented ship and its related helper functions

---
+++ src/ship.py
@@ -0,0 +1,26 @@
+from src.config import ShipConfig
+
+class Ship:
+    """
+    Represents a ship in the game with functionalities for tracking hits and checks if it's destroyed.
+    """
+    def __init__(self, ship_id, player_id, config: ShipConfig):
+        self.ship_id = ship_id
+        self.player_id = player_id
+        self.config = config
+        self.remaining_hits = self.config.length * self.config.width
+
+    def hit(self):
+        """
+        Register a hit on the ship and reduce the remaining hits by 1.
+        """
+        self.remaining_hits -= 1
+
+    def is_alive(self):
+        """
+        Check if the ship is still alive based on remaining hits.
+
+        Returns:
+        bool: True if the ship is still alive (has remaining hits), False otherwise.
+        """
+        return self.remaining_hits > 0
